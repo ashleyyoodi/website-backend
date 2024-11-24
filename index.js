40ccc232fe0a3ab4f63d88786914702e54e0ca6d
yoodi <ashleyyoodikim@gmail.com>
2024-11-24 13:14:26 -0800

    rename db/db.js for debugging index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import * as db from "./db/index.js";
 
-export const app = express();
+const app = express();
 const port = 5000;
 
+app.use(express.json());
 app.use(cors());
 
 app.get('/blog-posts', async (request, response) => {
+  console.log("hit /blog-posts");
   const result = await db.query('SELECT * FROM blog_post');
   response.status(200).json(result.rows);
 });
