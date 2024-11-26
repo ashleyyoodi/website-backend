1ca755101d93274fe7ff8aab8bdff2c09f1531f2
yoodi <ashleyyoodikim@gmail.com>
2024-11-25 19:32:26 -0800

    add /admin/authorize api

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const port = 5000;
 app.use(express.json());
 app.use(cors());
 
+/* Blog Posts */
+
 app.get('/blog-posts', async (request, response) => {
   console.log("hit /blog-posts");
   const result = await db.query('SELECT * FROM blog_post ORDER BY created_date desc');
@@ -17,4 +19,13 @@ app.get('/blog-posts', async (request, response) => {
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+});
+
+
+/* Admin */
+
+app.get('/admin/authorize', async(request, response) => {
+  console.log("hit /admin/authorize");
+  const authorized = request.query.input == process.env.ADMIN_PASSWORD;
+  response.status(200).send(authorized);
 });
